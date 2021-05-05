d9fb2c83adc7eefd303a9c8eb0b46d5d08b30b61
Kyle Leduc <LeducKyleJ@gmail.com>
2021-05-05 15:50:30 -0600

    Add redirect to lobby if game hasn't been played

--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 
 export default class ScoreBoard extends Component {
   render() {
@@ -6,10 +7,12 @@ export default class ScoreBoard extends Component {
     const { players } = this.props.gameSettings;
     return (
       <div>
+        {Object.keys(topScorer).length === 0 ? <Redirect to="/" /> : undefined}
         <h2>{`${topScorer.name} wins with ${topScorer.score} points!`}</h2>
         {players.map(player => {
           return <div key={player.id}>{`${player.name} - ${player.score}`}</div>
         })}
+        <Link to='/'>New Game</Link>
       </div>
     );
   }
