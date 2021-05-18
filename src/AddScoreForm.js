62b5a6ea084836e0f3296fe6465a1f31f739b63c
Kyle Leduc <LeducKyleJ@gmail.com>
2021-05-18 16:54:04 -0600

    Style AddScoreForm using JSS
    - disable text-shadow & text-stroke
    - change form to TextField to remove arrows

--- src/AddScoreForm.js
+++ src/AddScoreForm.js
@@ -1,62 +1,73 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
-import { Input } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
+import { withStyles } from '@material-ui/styles';
 
-export default class AddScoreForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { score: '' };
-  }
-  handleSubmit = evt => {
-    evt.preventDefault();
-    const { id, addScore, scoreFormToggle } = this.props;
-    const { score } = this.state;
-    if (score > 0) {
-      addScore(id, score);
-    } else {
-      scoreFormToggle(id);
+const styles = {
+  AddScoreForm: {
+    display: 'flex',
+    textShadow: 'none',
+    textStroke: '0',
+  },
+};
+
+export default withStyles(styles)(
+  class AddScoreForm extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { score: '' };
     }
-  };
-  handleChange = evt => {
-    const { name, value } = evt.target;
-    const safeParseInt = score => {
-      return parseInt(score.replace(/\D/, ''));
+    handleSubmit = evt => {
+      evt.preventDefault();
+      const { id, addScore, scoreFormToggle } = this.props;
+      const { score } = this.state;
+      if (score > 0) {
+        addScore(id, score);
+      } else {
+        scoreFormToggle(id);
+      }
     };
-    this.setState({
-      [name]: safeParseInt(value),
-    });
-  };
-  render() {
-    return (
-      <div>
-        <form className="AddScoreForm" onSubmit={this.handleSubmit}>
-          <Input
-            className="AddScoreForm-Input"
-            id="score"
-            name="score"
-            type="number"
-            min="0"
-            step="1"
-            autoFocus
-            autoComplete="off"
-            placeholder="Enter the score"
-            value={this.state.score}
-            onChange={this.handleChange}
-          />
-          <Button
-            type="submit"
-            className="AddScoreForm-button"
-            style={{ backgroundColor: 'blue', color: 'white' }}>
-            +
-          </Button>
-        </form>
-        <Button
-          onClick={() => this.props.scoreFormToggle(this.props.id)}
-          className="AddScoreForm-button"
-          style={{ backgroundColor: 'red', color: 'white' }}>
-          X
-        </Button>
-      </div>
-    );
-  }
-}
+    handleChange = evt => {
+      const { name, value } = evt.target;
+      const safeParseInt = score => {
+        return parseInt(score.replace(/\D/, ''));
+      };
+      this.setState({
+        [name]: safeParseInt(value),
+      });
+    };
+    render() {
+      const { classes } = this.props;
+      return (
+        <div>
+          <form className={classes.AddScoreForm} onSubmit={this.handleSubmit}>
+            <TextField
+              className={classes.AddScoreFormInput}
+              id="score"
+              name="score"
+              variant="filled"
+              size="small"
+              autoFocus
+              autoComplete="off"
+              label="Enter the Score"
+              value={this.state.score}
+              onChange={this.handleChange}
+            />
+            <Button
+              type="submit"
+              size="small"
+              style={{ backgroundColor: 'blue', color: 'white' }}>
+              +
+            </Button>
+            <Button
+              onClick={() => this.props.scoreFormToggle(this.props.id)}
+              size="small"
+              style={{ backgroundColor: 'red', color: 'white' }}>
+              X
+            </Button>
+          </form>
+        </div>
+      );
+    }
+  },
+);
