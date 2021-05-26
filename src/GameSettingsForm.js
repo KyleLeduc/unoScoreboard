888138a1b9e1f69cf8515d94464581d7e92db0d0
Kyle Leduc <LeducKyleJ@gmail.com>
2021-05-26 14:09:02 -0600

    Add withRouter to GameSettingsForm
    - use history.push to change to GameWindow when game starts

--- src/GameSettingsForm.js
+++ src/GameSettingsForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import NewPlayerForm from './NewPlayerForm';
 import Player from './Player';
 import Button from '@material-ui/core/Button';
@@ -7,87 +8,93 @@ import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 
 import styles from './styles/GameSettingsFormStyles';
 
-export default withStyles(styles)(
-  class GameSettingsForm extends Component {
-    constructor(props) {
-      super(props);
-      this.state = { winScore: 500, players: [] };
-    }
-    addPlayer = newPlayer => {
-      const newPlayerList = [...this.state.players, newPlayer];
-      this.setState(st => ({
-        players: [...newPlayerList],
-      }));
-    };
-    removePlayer = id => {
-      const newPlayerList = this.state.players.filter(
-        player => player.id !== id,
-      );
-      this.setState({
-        players: newPlayerList,
-      });
-    };
-    handleSubmit = evt => {
-      evt.preventDefault();
-      const gameSettings = {
-        ...this.state,
-        winScore: parseInt(this.state.winScore),
+export default withRouter(
+  withStyles(styles)(
+    class GameSettingsForm extends Component {
+      constructor(props) {
+        super(props);
+        this.state = { winScore: 500, players: [] };
+      }
+      addPlayer = newPlayer => {
+        const newPlayerList = [...this.state.players, newPlayer];
+        this.setState(st => ({
+          players: [...newPlayerList],
+        }));
       };
-      this.props.startGame(gameSettings);
-    };
-    handleChange = evt => {
-      const { name, value } = evt.target;
-      this.setState({
-        [name]: value,
-      });
-    };
-    renderPlayerList = () => {
-      return this.state.players.map(player => {
-        const { key, id, name } = player;
-        return (
-          <Player
-            key={key}
-            id={id}
-            name={name}
-            handleRemove={this.removePlayer}
-          />
+      removePlayer = id => {
+        const newPlayerList = this.state.players.filter(
+          player => player.id !== id,
         );
-      });
-    };
-    render() {
-      const gameReady = this.state.players.length > 1;
-      const { classes } = this.props;
-      return (
-        <ValidatorForm
-          ref="form"
-          instantValidate={true}
-          className={classes.gameSettingsForm}
-          onSubmit={this.handleSubmit}>
-          <TextValidator
-            className={classes.winScoreInput}
-            id="winScore"
-            name="winScore"
-            variant="filled"
-            size="small"
-            validators={['required', 'isNumber', 'isPositive']}
-            errorMessages={[
-              'Enter a winning score',
-              'Must be a number',
-              'Number must be positive',
-            ]}
-            autoComplete="off"
-            inputProps={{ inputMode: 'numeric' }}
-            label="Winning Score"
-            value={this.state.winScore}
-            onChange={this.handleChange}
-          />
-          <NewPlayerForm addPlayer={this.addPlayer} gameReady={gameReady} />
-          {this.renderPlayerList()}
-          <Button className={classes.startButton} disabled={!gameReady} type="submit">
-            Start Game
-          </Button>
-        </ValidatorForm>
-      );
-    }
-  },
+        this.setState({
+          players: newPlayerList,
+        });
+      };
+      handleSubmit = evt => {
+        evt.preventDefault();
+        const gameSettings = {
+          ...this.state,
+          winScore: parseInt(this.state.winScore),
+        };
+        this.props.startGame(gameSettings);
+        this.props.history.push('/game');
+      };
+      handleChange = evt => {
+        const { name, value } = evt.target;
+        this.setState({
+          [name]: value,
+        });
+      };
+      renderPlayerList = () => {
+        return this.state.players.map(player => {
+          const { key, id, name } = player;
+          return (
+            <Player
+              key={key}
+              id={id}
+              name={name}
+              handleRemove={this.removePlayer}
+            />
+          );
+        });
+      };
+      render() {
+        const gameReady = this.state.players.length > 1;
+        const { classes } = this.props;
+        return (
+          <ValidatorForm
+            ref="form"
+            instantValidate={true}
+            className={classes.gameSettingsForm}
+            onSubmit={this.handleSubmit}>
+            <TextValidator
+              className={classes.winScoreInput}
+              id="winScore"
+              name="winScore"
+              variant="filled"
+              size="small"
+              validators={['required', 'isNumber', 'isPositive']}
+              errorMessages={[
+                'Enter a winning score',
+                'Must be a number',
+                'Number must be positive',
+              ]}
+              autoComplete="off"
+              inputProps={{ inputMode: 'numeric' }}
+              label="Winning Score"
+              value={this.state.winScore}
+              onChange={this.handleChange}
+            />
+            <NewPlayerForm addPlayer={this.addPlayer} gameReady={gameReady} />
+            {this.renderPlayerList()}
+            <Button
+              className={classes.startButton}
+              disabled={!gameReady}
+              type="submit">
+              Start Game
+            </Button>
+          </ValidatorForm>
+        );
+      }
+    },
+  ),
 );
