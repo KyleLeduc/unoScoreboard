548177b56d1f6185dc32ab256b006665c2a4343a
Kyle Leduc <LeducKyleJ@gmail.com>
2021-05-26 08:45:05 -0600

    Remove commented out code from Lobby.js

--- src/Lobby.js
+++ src/Lobby.js
@@ -1,28 +1,13 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/styles';
 import GameSettingsForm from './GameSettingsForm';
-// import Player from './Player';
 
 import styles from './styles/LobbyStyles';
 
 export default withStyles(styles)(
   class Lobby extends Component {
-    // renderPlayerList = () => {
-    //   return this.props.gameSettings.players.map(player => {
-    //     const { key, id, name } = player;
-    //     return (
-    //       <Player
-    //         key={key}
-    //         id={id}
-    //         name={name}
-    //         handleRemove={this.props.removePlayer}
-    //       />
-    //     );
-    //   });
-    // };
-
     render() {
-      const { classes, gameSettings, addPlayer, updateWinScore, startGame } =
+      const { classes, gameSettings, updateWinScore, startGame } =
         this.props;
 
       return (
@@ -32,11 +17,9 @@ export default withStyles(styles)(
           <GameSettingsForm
             winScore={gameSettings.winScore}
             updateWinScore={updateWinScore}
-            // addPlayer={addPlayer}
             gameSettings={gameSettings}
             startGame={startGame}
           />
-          {/* {this.renderPlayerList()} */}
         </div>
       );
     }
